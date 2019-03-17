4551b8e5e6d8cb49ad9fe47d21af97e2e9aef1fb
Michał Krul <michal@krul.pl>
2019-03-17 15:52:58 +0100

    Added lazy to ManaCurve

--- src/components/ManaCurve.js
+++ src/components/ManaCurve.js
@@ -1,37 +1,47 @@
-import React from 'react';
-import { Checkbox } from 'semantic-ui-react';
-import { ManaCurveChart } from './ManaCurveChart';
+import React, { lazy, Suspense } from "react";
+import { Checkbox, Loader } from "semantic-ui-react";
+
+const LazyManaCurveChart = lazy(() => import("./ManaCurveChart"));
 
 export class ManaCurve extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { colorSafe: false };
-  }
-  handlColor = () => {
-    this.setState(prevState => {
-      return { colorSafe: !prevState.colorSafe };
-    });
-  };
-  render() {
-    return (
-      <React.Fragment>
-        <Checkbox
-          checked={this.props.isManaVisible}
-          onChange={() => this.props.setManaVisible(!this.props.isManaVisible)}
-          toggle
-          label="Krzywa many"
-        />
-        {this.props.isManaVisible && (
-          <React.Fragment>
-            <ManaCurveChart colorSafe={this.state.colorSafe} {...this.props} />
-            <Checkbox
-              checked={this.state.colorSafe}
-              onChange={() => this.handlColor()}
-              label="Bezpieczne kolory"
-            />
-          </React.Fragment>
-        )}
-      </React.Fragment>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = { colorSafe: false };
+	}
+	handlColor = () => {
+		this.setState(prevState => {
+			return { colorSafe: !prevState.colorSafe };
+		});
+	};
+	render() {
+		return (
+			<React.Fragment>
+				<Checkbox
+					checked={this.props.isManaVisible}
+					onChange={() => this.props.setManaVisible(!this.props.isManaVisible)}
+					toggle
+					label="Krzywa many"
+				/>
+				{this.props.isManaVisible && (
+					<Suspense
+						fallback={
+							<Loader style={{ marginTop: 75 }} active inline="centered" />
+						}
+					>
+						<React.Fragment>
+							<LazyManaCurveChart
+								colorSafe={this.state.colorSafe}
+								{...this.props}
+							/>
+							<Checkbox
+								checked={this.state.colorSafe}
+								onChange={() => this.handlColor()}
+								label="Bezpieczne kolory"
+							/>
+						</React.Fragment>
+					</Suspense>
+				)}
+			</React.Fragment>
+		);
+	}
 }
