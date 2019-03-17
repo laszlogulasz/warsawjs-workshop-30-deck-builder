c66f46349f2df5b2a1d0d3b58ea8c9a846181732
Michał Krul <michal@krul.pl>
2019-03-17 15:10:55 +0100

    Added debounce to CardFilter

--- src/components/CardFilter.js
+++ src/components/CardFilter.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import { Input, Dropdown } from 'semantic-ui-react';
+import React from "react";
+import { Input, Dropdown } from "semantic-ui-react";
+import { debounce } from "throttle-debounce";
 
 export class CardFilter extends React.Component {
-  render() {
-    return (
-      <Input
-        action={
-          <Dropdown
-            onChange={(e, { value }) => this.props.setCardsTypeFilter(value)}
-            button
-            basic
-            //new object every time
-            options={[
-              { key: 'ALL', text: 'Wszystkie', value: 'ALL' },
-              { key: 'MINION', text: 'Stronnik', value: 'MINION' },
-              { key: 'SPELL', text: 'Zaklęcie', value: 'SPELL' },
-              { key: 'WEAPON', text: 'Broń', value: 'WEAPON' },
-            ]}
-            value={this.props.cardsTypeFilter}
-          />
-        }
-        onChange={(e, { value }) => this.props.setSearchText(value)}
-        icon="search"
-        iconPosition="left"
-        fluid
-        value={this.props.searchText}
-        placeholder="Szukaj..."
-      />
-    );
-  }
+	onInputChange = debounce(300, this.props.setSearchText);
+	render() {
+		return (
+			<Input
+				action={
+					<Dropdown
+						onChange={(e, { value }) => this.props.setCardsTypeFilter(value)}
+						button
+						basic
+						//new object every time
+						options={[
+							{ key: "ALL", text: "Wszystkie", value: "ALL" },
+							{ key: "MINION", text: "Stronnik", value: "MINION" },
+							{ key: "SPELL", text: "Zaklęcie", value: "SPELL" },
+							{ key: "WEAPON", text: "Broń", value: "WEAPON" }
+						]}
+						value={this.props.cardsTypeFilter}
+					/>
+				}
+				onChange={(e, { value }) => this.onInputChange(value)}
+				icon="search"
+				iconPosition="left"
+				fluid
+				placeholder="Szukaj..."
+			/>
+		);
+	}
 }
